7dd5a9277621b55362cebaba4b44bfd3a616458d
Bárbara <barbisuperlogico7@gmail.com>
2021-07-05 21:29:14 -0300

    subiendo actividad php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Mi nombre es Bárbara Galiotti.";
+        echo "<br>";
+
+        echo "Mi edad es 33 años.";
+        echo "<br>";
+
+        echo "Mi hobbie es escuchar música y tocar el piano.";
+        echo "<br>";
+
+        echo "Mi editor de código preferido es Visual Studio Code.";
+        echo "<br>";
+
+        echo "Mi sistema operativo es ".PHP_OS;
+    ?>
+
+</body>
+</html>
